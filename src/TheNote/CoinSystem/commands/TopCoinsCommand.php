6f17705165bb7043ecc450612e29936003fe6cd5
TheNote <38653750+note3crafter@users.noreply.github.com>
2023-07-25 14:47:53 +0200

    Add files via upload

--- src/TheNote/CoinSystem/commands/TopCoinsCommand.php
+++ src/TheNote/CoinSystem/commands/TopCoinsCommand.php
@@ -10,7 +10,7 @@
 
 namespace TheNote\CoinSystem\commands;
 
-use TheNote\CoinSystem\CoinAPI;
+use pocketmine\console\ConsoleCommandSender;
 use TheNote\CoinSystem\Main;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -28,7 +28,7 @@ class TopCoinsCommand extends Command implements Listener
     {
         $this->plugin = $plugin;
         $api = new CoreAPI();
-        parent::__construct("topcoins", $api->getCommandPrefix("Prefix") . $plugin->getCommandPrefix("TopCoinsDescription"), "/topcoins");
+        parent::__construct("topcoins", $api->getCommandPrefix("Prefix") . $plugin->getCommandPrefix("TopCoinsDescription"), "/topcoins (side)");
         $this->setPermission(Permissions::$defaultperm);
     }
 
@@ -39,18 +39,39 @@ class TopCoinsCommand extends Command implements Listener
             $sender->sendMessage($api->getCommandPrefix("Error") . $api->getCommandPrefix("CommandIngame"));
             return false;
         }
-        $moneys = new Config(Main::getInstance()->getDataFolder() . "Coins.yml", Config::YAML);
-        $moneys = $moneys->get("coins", []);
-        $money = [];
-        foreach ($moneys as $p => $m) {
-            $money[] = ["Coins" => $m, "Player" => $p];
+        if (empty($args[0])) {
+            $sender->sendMessage($api->getCommandPrefix("Info") . $this->plugin->getLang($sender->getName(), "TopCoinsUsage"));
+            return false;
         }
-        rsort($money);
-        $sender->sendMessage($this->plugin->getLang($sender->getName(), "TopCoins"));
-        $money = array_slice($money, 0, 10);
-        foreach ($money as $rank => $data) {
-            $datas = round($data["Coins"], 2);
-            $sender->sendMessage("§d" . ($rank + 1) . "§f. " . $data["Player"] . " §f:§d " . $datas . " Coins");
+        $moneys = new Config(Main::getInstance()->getDataFolder() . "Coins.yml", Config::YAML);
+        $moneyData = $moneys->get("coins", []);
+        $sortedMoneyData = [];
+        foreach ($moneyData as $playerName => $moneyAmount) {
+            $sortedMoneyData[] = ["Player" => $playerName, "Coins" => $moneyAmount];
+        }
+        usort($sortedMoneyData, function ($a, $b) {
+            return $b["Coins"] - $a["Coins"];
+        });
+        $pageHeight = ($sender instanceof ConsoleCommandSender) ? 48 : 6;
+        $chunkedMoney = array_chunk($sortedMoneyData, $pageHeight);
+        $maxPageNumber = count($chunkedMoney);
+        if ($args[0] > $maxPageNumber) {
+            $sender->sendMessage($api->getCommandPrefix("Error") . str_replace("{maxpage}", $maxPageNumber, $this->plugin->getLang($sender->getName(), "TopCoinsPageError")));
+            return false;
+        }
+        $pageNumber = isset($args[0]) && is_numeric($args[0]) && $args[0] > 0 ? min($args[0], $maxPageNumber) : 1;
+        $sender->sendMessage($api->getLang($sender->getName(), "TopCoins"));
+        $msg = str_replace("{page}", $pageNumber, $this->plugin->getLang($sender->getName(), "TopCoinsPageList"));
+        $sender->sendMessage(str_replace("{maxpage}", $maxPageNumber, $msg));
+        if (isset($chunkedMoney[$pageNumber - 1])) {
+            $pageData = $chunkedMoney[$pageNumber - 1];
+            $rank = $pageNumber === 1 ? 0 : ($pageNumber - 1) * $pageHeight;
+            foreach ($pageData as $data) {
+                $playerName = $data["Player"];
+                $moneyAmount = round(intval($data["Coins"]), 2);
+                $sender->sendMessage("§d" . ($rank + 1) . "§f. " . $playerName . " §f:§d " . $moneyAmount . " Coins");
+                $rank++;
+            }
         }
         return true;
     }
